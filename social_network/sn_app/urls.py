553066728ceb26d52b151be979b8eefc5d1910b4
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-02-14 09:42:29 +0600

    ekinshi

--- social_network/sn_app/urls.py
+++ social_network/sn_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin')
+
 ]
--- social_network/sn_app/views.py
+++ social_network/sn_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 def index(request):
     return render(request, 'index.html')
@@ -25,11 +26,42 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                #Log user in and redirect to settings page
+                #Create a Profile object for the new user
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect(signup)
         else:
             messages.info(request, 'Passwords dont match!')
             return redirect('signup')
 
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Derekter qate!')
+            return redirect('signin')
+
+
+    else:
+        return render(request, 'signin.html')
+
+
+
+
 
 # Create your views here.
