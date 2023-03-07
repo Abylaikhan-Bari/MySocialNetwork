edd2f4b78d8f892a4f3cc0ddc545e101dcb6096c
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-03-07 09:26:17 +0600

    2:17

--- social_network/sn_app/views.py
+++ social_network/sn_app/views.py
@@ -12,8 +12,32 @@ def index(request):
     return render(request, 'index.html')
 
 @login_required(login_url='signin')
+
 def settings(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
 
