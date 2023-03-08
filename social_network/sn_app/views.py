fa3c43347ff6e66d3ddde6b96d1e5495869d52f7
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-03-08 23:23:13 +0600

    4:19

--- social_network/sn_app/views.py
+++ social_network/sn_app/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 
 @login_required(login_url='signin')
@@ -11,9 +12,22 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
     posts = Post.objects.all()
 
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_list})
 
 
 @login_required(login_url='signin')
