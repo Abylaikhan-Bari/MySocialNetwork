8a01f7b6c3a5a647c6e2b46eeeac788895ac0c79
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-03-08 19:58:05 +0600

    2:28:43

--- social_network/sn_app/models.py
+++ social_network/sn_app/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
-    caption = models.TextField
+    caption = models.TextField(default='caption...')
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
 
