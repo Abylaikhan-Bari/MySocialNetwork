c94038dc8fa40c40c8177ac2fd7c1345d901b08f
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-03-08 22:17:01 +0600

    3:37:27

--- social_network/sn_app/urls.py
+++ social_network/sn_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
