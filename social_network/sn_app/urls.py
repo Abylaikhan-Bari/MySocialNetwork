eb6cc974bedecd839eb073fb153c5f95a40ba4d2
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-03-08 21:39:28 +0600

    3:20:56

--- social_network/sn_app/urls.py
+++ social_network/sn_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns=[
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout')
--- social_network/sn_app/views.py
+++ social_network/sn_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 @login_required(login_url='signin')
@@ -14,6 +14,7 @@ def index(request):
     posts = Post.objects.all()
 
     return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
+
 
 @login_required(login_url='signin')
 def upload(request):
@@ -28,6 +29,26 @@ def upload(request):
     else:
         return redirect('/')
 
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
 
 @login_required(login_url='signin')
 def settings(request):
