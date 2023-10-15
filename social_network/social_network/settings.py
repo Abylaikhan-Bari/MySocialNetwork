caa7a6576592d166782f922a0e5c57d92fd7b775
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-15 10:31:07 +0600

    Added security feature

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -75,14 +75,23 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysocialnetwork',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or the address where your MySQL server is running
+        'PORT': '3306',      # The default MySQL port
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
